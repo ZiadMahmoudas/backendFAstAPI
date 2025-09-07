cb7d82269b6228d61bd2f35ded6f58406c3f1e1a
Ziad Elmohager <ziadbobo78@gmail.com>
2025-09-07 20:29:12 +0300

    change

--- api/database.py
+++ api/database.py
@@ -2,6 +2,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = "sqlite+aiosqlite:///./test.db"
+# DATABASE_URL = "postgresql+asyncpg://postgres:YourPassword@db.zrzfbhivkdmjirqvqkwm.supabase.co:5432/postgres"
 
 engine = create_async_engine(DATABASE_URL, echo=True, future=True)
 
