afc944528b23430b760d2973c0ce248b4039b2ba
Ziad Elmohager <ziadbobo78@gmail.com>
2025-09-08 15:14:33 +0300

    ؤ

--- api/main.py
+++ api/main.py
@@ -21,8 +21,8 @@ async def init_models():
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
 
-@app.on_event("startup")
-async def on_startup():
-    await init_models()
+# @app.on_event("startup")
+# async def on_startup():
+#     await init_models()
 
-handler = Mangum(app)
+# handler = Mangum(app)
