dac3a2ace5091420d0b9c0569d9067269e28e140
Ziad Elmohager <ziadbobo78@gmail.com>
2025-09-09 14:40:59 +0300

    ؤ

--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from api.routers import heroes
 from api.database import init_models
 import logging
 logging.basicConfig(level=logging.DEBUG)
-
+import asyncio
 app = FastAPI(title="Heroes API (Async)", version="6.0")
 
 app.include_router(heroes.router)
@@ -17,17 +17,26 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# دالة لتبدأ تشغيل الجداول عند بدء التطبيق
-@app.on_event("startup")
-async def startup():
-    try:
-        logging.debug("Initializing the database tables...")
-        await init_models()  # تهيئة الجداول عند بدء التطبيق
-    except Exception as e:
-        logging.error(f"Error during startup: {e}")
+# # دالة لتبدأ تشغيل الجداول عند بدء التطبيق
+# @app.on_event("startup")
+# async def startup():
+#     try:
+#         logging.debug("Initializing the database tables...")
+#         await init_models()  # تهيئة الجداول عند بدء التطبيق
+#     except Exception as e:
+#         logging.error(f"Error during startup: {e}")
 
+async def create_tables():
+    print("جاري إنشاء جداول قاعدة البيانات...")
+    await init_models()
+    print("✅ تم إنشاء الجداول بنجاح!")
+
+if __name__ == "__main__":
+    asyncio.run(create_tables())
 
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World"}
+
+
