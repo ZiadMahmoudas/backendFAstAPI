81bf742d3687e87a3421e632f21015a1a2832805
Ziad Elmohager <ziadbobo78@gmail.com>
2025-09-09 10:38:11 +0300

    ؤ

--- api/main.py
+++ api/main.py
@@ -16,14 +16,14 @@ app.add_middleware(
 )
 
 # دالة لتبدأ تشغيل الجداول عند بدء التطبيق
-@app.on_event("startup")
-async def startup():
-    print("Initializing the database tables...")
-    await init_models()  # تهيئة الجداول عند بدء التطبيق
+# @app.on_event("startup")
+# async def startup():
+#     print("Initializing the database tables...")
+#     await init_models()  # تهيئة الجداول عند بدء التطبيق
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("api.main:app", host="127.0.0.1", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("api.main:app", host="127.0.0.1", port=8000, reload=True)
 
 @app.get("/")
 def read_root():
